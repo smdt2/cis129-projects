af2569fe3ac8099637f5f2f924f82f5c25b3627c
smdt2 <160813265+smdt2@users.noreply.github.com>
2024-02-21 22:47:10 -0700

    Update cis129_lab02_shortStory.py
    Added details about the file, name of the author and added a short story to the file.

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,8 @@
-print('Hello, Wolrd!')
+#Author: Sebastian Melendez
+#My first Python lab in my CIS129 course
+#Prints a simple short story
+print(There was once a kid whose dream was becoming like his father, a big tech company CEO) 
+print(He thought it would be impossible to overcome him due to his success and was scared he was going to be his shadow forever) 
+print(He took this to heart and learned whatever he could from his Father's company and started working on his in secret) 
+print(He later created a videogame using virtual Reality and broke the videogame market, creating a name for himself and getting the praise from his dad)
+print(They later merged their companies and worked together towards new goals together.)
