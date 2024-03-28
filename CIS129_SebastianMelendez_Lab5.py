6ff691a00168f25a1672ecad2a3601b8c2c95209
smdt2 <160813265+smdt2@users.noreply.github.com>
2024-03-27 22:46:13 -0700

    Add files via upload

---
+++ CIS129_SebastianMelendez_Lab5.py
@@ -0,0 +1,25 @@
+keep_going = 'y'
+# Set variables and first while loop
+while keep_going == 'y':
+    total_bottles = 0
+    counter = 1
+    today_bottles = 0
+    total_payout = 0
+
+    # The variables are calculated
+
+    while counter <= 7:
+        today_bottles = int(input(f'Enter number of bottles returned for day #{counter}: '))
+        counter = counter + 1
+        total_bottles = total_bottles + today_bottles
+        total_payout = total_bottles * .10
+
+    #Prints out total_bottles and total_payout
+    print(f'\nThe total number of bottles collected is {total_bottles}')
+    print(f'The total paid out is ${total_payout:.2f}\n')
+
+    #Asks if they wanna keep going or not
+    keep_going = input('Do you want to enter another week\'s worth of data?\n(Enter y or n): ')
+    print()
+
+keep_going = 'y'
